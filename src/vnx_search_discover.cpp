bab600c7def7fe4db3beff86685fb6db9a2e2834
Max Wittal <max.wittal@mwittal.de>
2020-05-29 21:25:47 +0200

    added vnx_search_discover

---
+++ src/vnx_search_discover.cpp
@@ -0,0 +1,68 @@
+/*
+ * vnx_search_discover.cpp
+ *
+ *  Created on: May 29, 2020
+ *      Author: mad
+ */
+
+#include <vnx/search/SearchEngineClient.hxx>
+
+#include <vnx/Config.h>
+#include <vnx/Process.h>
+#include <vnx/Proxy.h>
+#include <vnx/PrettyPrinter.h>
+
+
+int main(int argc, char** argv)
+{
+	std::map<std::string, std::string> options;
+	options["s"] = "server";
+	options["l"] = "limit";
+	options["p"] = "page";
+	options["server"] = "backend server url";
+	options["limit"] = "max results";
+	options["page"] = "page index";
+	
+	vnx::init("vnx_search_discover", argc, argv, options);
+	
+	std::string server = ".vnx_search_backend.sock";
+	int32_t limit = 100;
+	uint32_t page = 0;
+	
+	vnx::read_config("server", server);
+	vnx::read_config("limit", limit);
+	vnx::read_config("page", page);
+	
+	{
+		vnx::Handle<vnx::Proxy> proxy = new vnx::Proxy("Proxy", vnx::Endpoint::from_url(server));
+		proxy->forward_list.push_back("SearchEngine");
+		proxy.start_detached();
+	}
+	
+	std::vector<std::string> root_urls;
+	vnx::read_config("CrawlProcessor.root_urls", root_urls);
+	
+	vnx::log_info().out << "Got " << root_urls.size() << " root urls.";
+	
+	vnx::search::SearchEngineClient client("SearchEngine");
+	
+	auto result = client.get_domain_list(limit, limit >= 0 ? page * limit : 0);
+	
+	for(const auto& domain : result) {
+		bool found = false;
+		const auto host = domain["host"].to_string_value();
+		for(const auto& url : root_urls) {
+			if(url.find(host) != std::string::npos) {
+				found = true;
+				break;
+			}
+		}
+		if(!found) {
+			vnx::PrettyPrinter print(std::cout);
+			vnx::accept(print, domain);
+			std::cout << std::endl;
+		}
+	}
+	
+	vnx::close();
+}
