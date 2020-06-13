99e8184465efbb8c399342d01fb99df530bedea2
Max Wittal <max.wittal@mwittal.de>
2020-06-13 15:09:16 +0200

    small fix

--- src/vnx_search_query.cpp
+++ src/vnx_search_query.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
 	
 	vnx::init("vnx_search_query", argc, argv, options);
 	
-	std::string server = ".vnx_search_backend.sock";
+	std::string server = ".vnx_search_engine.sock";
 	std::vector<std::string> words;
 	int32_t limit = 10;
 	uint32_t page = 0;
