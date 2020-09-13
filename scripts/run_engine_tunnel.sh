6522234a9f1160c2526e0f744ade42b829fdb335
Max <mad@PowerEdge-R721.Speedport_W_724V_Typ_A_05011603_06_003>
2020-09-13 23:39:53 +0200

    scripts

---
+++ scripts/run_engine_tunnel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+	ssh -i ~/mad-ovh -C -N -R 8989:localhost:8989 ubuntu@fcknwo.com
+	sleep 1
+done
+
--- scripts/run_frontend_tunnel.sh
+++ scripts/run_frontend_tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-	ssh -i ~/mad-ovh -C -N ubuntu@51.89.4.252 -L 5555:51.89.4.252:5555
+	ssh -i ~/mad-ovh -C -N ubuntu@fcknwo.com -L 5555:51.89.4.252:5555
 	sleep 1
 done
 
