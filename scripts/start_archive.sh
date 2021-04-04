f318781c9927b1ebc67f79a7c960f983cf8aea5b
Max <mad@PowerEdge-R720.Speedport_W_724V_Typ_A_05011603_06_003>
2021-04-04 20:21:28 +0200

    scripts

--- scripts/start_archive.sh
+++ scripts/start_archive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-screen -S archive -d -m ./build/vnx_search_archive -c config/local/
+screen -S archive -d -m ./build/vnx_search_archive -c config/local/ config/archive/ -f :9999
 
--- scripts/start_crawler.sh
+++ scripts/start_crawler.sh
@@ -2,5 +2,5 @@
 
 killall vnx_search_crawl
 
-screen -S crawler -d -m ./build/vnx_search_crawl -c config/alternate/ -f :9999 -i $@
+screen -S crawler -d -m ./build/vnx_search_crawl -c config/alternate/ -s 10.1.7.21:5656 -f :9999 -i $@
 
